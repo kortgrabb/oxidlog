99f44395313545b894ae355bb61876dbc62d0d45
kortgrabb <timherdemark86@gmail.com>
2025-01-24 15:55:18 +0100

    feat: add case-sensetive flag to search

--- src/commands/search.rs
+++ src/commands/search.rs
@@ -4,8 +4,7 @@ use crate::{
     utils::{self, TagMatch},
 };
 
-// TODO: Add date-filter, case-sensitive search, regex search
-
+// TODO: add regex search
 #[derive(clap::Args, Clone)]
 pub struct SearchArgs {
     pub query: String,
@@ -19,12 +18,66 @@ pub struct SearchArgs {
     pub fuzzy: bool,
     #[clap(short, long)]
     pub all: bool,
+    #[clap(short, long)]
+    pub case_sensitive: bool,
+}
+
+fn check_date_range(
+    entry_date: chrono::NaiveDate,
+    from: &Option<String>,
+    to: &Option<String>,
+) -> bool {
+    if let Some(from_date) = from {
+        let date = utils::parse_date(from_date);
+        if date > entry_date {
+            return false;
+        }
+    }
+
+    if let Some(to_date) = to {
+        let date = utils::parse_date(to_date);
+        if date < entry_date {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn check_content_match(content: &str, term: &str, fuzzy: bool) -> bool {
+    if fuzzy {
+        utils::fuzzy_match(content, term)
+    } else {
+        content.contains(term)
+    }
+}
+
+fn print_results(found: Vec<String>, term: &str) {
+    println!("{} entries found", found.len());
+    found.iter().for_each(|e| {
+        if term.is_empty() {
+            println!("{}", e);
+        } else {
+            let highlighted =
+                e.to_lowercase()
+                    .match_indices(&term)
+                    .fold(e.to_string(), |acc, (i, _)| {
+                        let orig_match = &acc[i..i + term.len()];
+                        acc.replacen(orig_match, &format!("\x1b[42m{}\x1b[0m", orig_match), 1)
+                    });
+            println!("{}", highlighted);
+        }
+    });
 }
 
 pub fn execute(journal: &Journal, args: SearchArgs, config: &Config) -> JotResult<()> {
-    let term = args.query.to_lowercase();
+    let term = if args.case_sensitive {
+        args.query
+    } else {
+        args.query.to_lowercase()
+    };
+
     let entries = journal.get_entries();
-    let do_fuzzy = args.fuzzy;
 
     if entries.is_empty() {
         println!("No entries found.");
@@ -32,28 +85,14 @@ pub fn execute(journal: &Journal, args: SearchArgs, config: &Config) -> JotResul
         let found: Vec<String> = entries
             .iter()
             .filter(|e| {
-                let content = e.body.to_lowercase();
-
-                let content_matches = if do_fuzzy {
-                    utils::fuzzy_match(&content, &term)
+                let content = if args.case_sensitive {
+                    e.body.clone()
                 } else {
-                    content.contains(&term)
+                    e.body.to_lowercase()
                 };
 
-                if let Some(from) = &args.from {
-                    let date = utils::parse_date(from);
-                    if date > e.date {
-                        return false;
-                    }
-                }
-
-                if let Some(to) = &args.to {
-                    let date = utils::parse_date(to);
-
-                    if date < e.date {
-                        return false;
-                    }
-                };
+                let content_matches = check_content_match(&content, &term, args.fuzzy);
+                let dates_match = check_date_range(e.date, &args.from, &args.to);
 
                 let match_type = if args.all {
                     TagMatch::All
@@ -61,7 +100,7 @@ pub fn execute(journal: &Journal, args: SearchArgs, config: &Config) -> JotResul
                     TagMatch::Any
                 };
 
-                utils::do_tags_match(
+                let tags_match = utils::do_tags_match(
                     &args
                         .tags
                         .iter()
@@ -69,20 +108,14 @@ pub fn execute(journal: &Journal, args: SearchArgs, config: &Config) -> JotResul
                         .collect::<Vec<_>>(),
                     &e.tags,
                     match_type,
-                ) && content_matches
+                );
+
+                content_matches && dates_match && tags_match
             })
             .map(|e| utils::format_entry(e, config.journal_cfg.clone()))
             .collect();
 
-        println!("{} entries found", found.len());
-        found.iter().for_each(|e| {
-            if term.is_empty() {
-                println!("{}", e);
-            } else {
-                let highlighted = e.replace(&term, &format!("\x1b[42m{}\x1b[0m", &term));
-                println!("{}", highlighted);
-            }
-        });
+        print_results(found, &term);
     }
 
     Ok(())
